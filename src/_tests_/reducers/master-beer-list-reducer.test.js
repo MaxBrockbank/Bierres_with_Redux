8c59b657688da58c345149c4990f0d83826cb141
Max Brockbank <maxbrockbank1999@gmail.com>
2021-02-19 10:28:02 -0800

    good fail on delete beer test on masterBeerListReducer

--- src/_tests_/reducers/master-beer-list-reducer.test.js
+++ src/_tests_/reducers/master-beer-list-reducer.test.js
@@ -11,12 +11,30 @@ const newBeer = {
   quantity: 9,
   id: 0
 }
+const currentState = {
+  0:{
+    name: "Czech Pilsner",
+    brand: "Buoy",
+    price: 4.99,
+    ABV: 6.2,
+    quantity: 9,
+    id: 0
+  },
+  1:{
+    name: "Hazy Lil' Thing ",
+    brand: "Sierra Nevada",
+    price: 5.99,
+    ABV: 5.1,
+    quantity: 20,
+    id: 1
+  }
+}
 
   it('will return default value of empty object if action type is null', () => {
     expect(masterBeerList({}, {type:null})).toEqual({});
   })
 
-  it('', ()=>{
+  it('will add new beer to state object with when type is ADD_BEER', ()=>{
     const {name, brand, price, ABV, quantity, id} = newBeer;
     action = {
       type: 'ADD_BEER',
@@ -35,6 +53,23 @@ const newBeer = {
         ABV: ABV,
         quantity: quantity, 
         id: id
+      }
+    })
+  })
+
+  it('will delete beer from state object when type is DELETE_BEER',()=>{
+    action = {
+      type:'DELETE_BEER',
+      id: 0
+    }
+    expect(masterBeerList(currentState, action)).toEqual({
+      1:{
+        name: "Hazy Lil' Thing ",
+        brand: "Sierra Nevada",
+        price: 5.99,
+        ABV: 5.1,
+        quantity: 20,
+        id: 1
       }
     })
   })
