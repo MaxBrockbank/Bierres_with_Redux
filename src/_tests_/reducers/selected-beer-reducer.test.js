174e9acf50e0aade7f256e1ba2cf80a319a0c539
Max Brockbank <maxbrockbank1999@gmail.com>
2021-02-19 12:13:10 -0800

    good fail on newSelect test for selectedBeerReducer

--- src/_tests_/reducers/selected-beer-reducer.test.js
+++ src/_tests_/reducers/selected-beer-reducer.test.js
@@ -1,7 +1,39 @@
 import selectedBeer from './../../Reducers/selected-beer-reducer';
 
 describe('selected beer reducer', ()=>{
+
+  let action;
+  const newSelect = {
+    name: "Classic Beer",
+    brand: "Good Beer Co.",
+    price: 10.99,
+    ABV: 8.2,
+    quantity: 124,
+    id: 0
+  }
+
   it('return default value of null when action type is not specified', ()=>{
     expect(selectedBeer(null, {type:null})).toEqual(null);
+  })
+
+  it('should return object of beer info when action type is NEW_SELECT', ()=>{
+    const {name, brand, price, ABV, quantity, id} = newSelect;
+    action ={
+      type:'NEW_SELECT',
+      name: name,
+      brand: brand,
+      price: price,
+      ABV:ABV,
+      quantity:quantity,
+      id:id
+    }
+    expect(selectedBeer(null, action)).toEqual({
+      name: name,
+      brand: brand,
+      price: price,
+      ABV:ABV,
+      quantity:quantity,
+      id:id
+    })
   })
 })
