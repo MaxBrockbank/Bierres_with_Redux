8f9803c82d82faf9857ff290ca85c19b2d166bef
Max Brockbank <maxbrockbank1999@gmail.com>
2021-02-19 09:39:56 -0800

    pass second test for formVisibleReducer

--- src/_tests_/reducers/form-visible-reducer.test.js
+++ src/_tests_/reducers/form-visible-reducer.test.js
@@ -5,7 +5,7 @@ describe('form visible reducer', () => {
   let action;
 
   it('make sure default return is false', ()=>{
-    expect(formVisible()).toEqual(false);
+    expect(formVisible(false, {type:null})).toEqual(false);
   })
 
   it('will toggle to true when passed an action of TOGGLE_FORM', ()=>{
