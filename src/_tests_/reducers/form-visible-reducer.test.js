c7cf4f989d15236fe0accf0937aae34a648f175d
Max Brockbank <maxbrockbank1999@gmail.com>
2021-02-19 09:16:10 -0800

    good fail for formVisibleReducer second test

--- src/_tests_/reducers/form-visible-reducer.test.js
+++ src/_tests_/reducers/form-visible-reducer.test.js
@@ -1,7 +1,18 @@
 import formVisible from './../../Reducers/form-visible-reducer';
 
 describe('form visible reducer', () => {
+
+  let action;
+
   it('make sure default return is false', ()=>{
     expect(formVisible()).toEqual(false);
+  })
+
+  it('will toggle to true when passed an action of TOGGLE_FORM', ()=>{
+    action = {
+      type: 'TOGGLE_FORM'
+    }
+
+    expect(formVisible(false, action)).toEqual(true);
   })
 })
